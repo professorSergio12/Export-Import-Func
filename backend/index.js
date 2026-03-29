b4dffa9df05a409821f1ddf966130b17235f86a5
Rohit Rawat <rohitrawat12092002@gmail.com>
2026-03-29 22:41:39 +0530

    solve cors error

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ connectionDB();
 // Middleware
 app.use(express.json());
 app.use(cors({
-    origin: ["http://localhost:5173", "http://localhost:3000"],
+    origin: ["http://localhost:5173", "https://export-import-func-1.onrender.com"],
     credentials: true,
 }));
 
